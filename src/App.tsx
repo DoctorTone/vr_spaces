6a9f78bd7db629e1950001e32d314017a37a8164
DoctorTone <tony@drt-software.com>
2023-09-18 19:23:38 +0100

    Add start camera position

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
 import { VRButton } from "@react-three/xr";
 import { Canvas } from "@react-three/fiber";
 import VRSpace from "./components/VRSpace";
+import { SCENE } from "./state/Config";
 
 const App = () => {
 	return (
 		<>
 			<VRButton />
-			<Canvas>
+			<Canvas camera={{ position: SCENE.cameraPosition }}>
 				<VRSpace />
 			</Canvas>
 		</>
---
+++ src/state/Config.tsx
@@ -0,0 +1,5 @@
+const SCENE = {
+	cameraPosition: [0, 1.7, 3],
+};
+
+export { SCENE };
