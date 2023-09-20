9e6fcf013280727ec410b5c01c8c6ee09549c4ff
DoctorTone <tony@drt-software.com>
2023-09-20 16:01:30 +0100

    Added car to scene

--- src/models/Exhibits.tsx
+++ src/models/Exhibits.tsx
@@ -1,12 +1,14 @@
 import React from "react";
+import { Lotus } from "./Lotus";
 
 const Exhibits = () => {
 	return (
 		<group>
-			<mesh>
-				<boxGeometry args={[1, 1, 1]} />
-				<meshStandardMaterial color={0x333333} />
-			</mesh>
+			<Lotus
+				position={[0, 0, 3.5]}
+				scale={[0.65, 0.65, 0.65]}
+				rotation-y={(Math.PI * 2) / 3}
+			/>
 		</group>
 	);
 };
